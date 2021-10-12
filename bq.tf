266f597ff528b1e1de8abb2b0151b0485cadf95b
looklikegarfield <looklikegarfieldd@gmail.com>
2021-10-12 15:27:51 +0530

    Update bq.tf

--- bq.tf
+++ bq.tf
@@ -23,3 +23,30 @@ resource "google_bigquery_dataset" "dataset" {
 resource "google_service_account" "bqowner" {
   account_id = "bqowner"
 }
+
+
+resource "google_bigquery_dataset" "dataset_2" {
+  dataset_id                  = "dataset_2"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
+
